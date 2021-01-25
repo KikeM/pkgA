bde494f05a7263782866aa36acb5e67f503fba63
Maximiliano Greco <mgreco@ets.es>
2021-01-25 11:22:10 +0100

    update

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ setup(
     name             = 'pkgA',
     packages         = find_packages(),
     install_requires = [
-        'pip-install-test>=0.4',
-        'pkgB'
+        'pip-install-test>0.4',
+        'pkgB@git+https://github.com/mmngreco/pkgB.git#egg=pkgB-0'
     ],
-    dependency_links = [
-        'git+https://github.com/mmngreco/pkgB.git#egg=pkgB-0']
 )
 
